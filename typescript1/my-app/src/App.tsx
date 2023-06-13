3694ff3d079176b228864a2bc8dd87d3c33b143e
Henry Ding <henryrding@yahoo.com>
2023-06-13 03:38:08 +0000

    Basic prop types: string, number, boolean, array, object.

--- typescript1/my-app/src/App.tsx
+++ typescript1/my-app/src/App.tsx
@@ -1,10 +1,34 @@
 import './App.css';
 import { Greet } from './components/Greet';
+import { Person } from './components/Person';
+import { PersonList } from './components/PersonList';
 
 function App() {
+  const personName = {
+    first: 'Naruto',
+    last: 'Uzumaki',
+  };
+
+  const nameList = [
+    {
+      first: 'Naruto',
+      last: 'Uzumaki',
+    },
+    {
+      first: 'Ichigo',
+      last: 'Kurosaki',
+    },
+    {
+      first: 'Anya',
+      last: 'Forger',
+    },
+  ];
+
   return (
     <div className="App">
-      <Greet name="Henry" />
+      <Greet name="Henry" messageCount={5} isLoggedIn={false} />
+      <Person name={personName} />
+      <PersonList names={nameList} />
     </div>
   );
 }
