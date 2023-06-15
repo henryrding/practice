d1b8d3795d9767002dff05281bc1a510e5e613e5
Henry Ding <henryrding@yahoo.com>
2023-06-15 05:56:31 +0000

    useReducer strict action types: discriminated unions.

--- typescript1/my-app/src/components/state/Counter.tsx
+++ typescript1/my-app/src/components/state/Counter.tsx
@@ -4,10 +4,17 @@ type CounterState = {
   count: number;
 };
 
-type CounterAction = {
-  type: string;
+// discriminated unions
+type UpdateAction = {
+  type: 'increment' | 'decrement';
   payload: number;
 };
+
+type ResetAction = {
+  type: 'reset';
+};
+
+type CounterAction = UpdateAction | ResetAction;
 
 const initialState = { count: 0 };
 
@@ -17,6 +24,8 @@ function reducer(state: CounterState, action: CounterAction) {
       return { count: state.count + action.payload };
     case 'decrement':
       return { count: state.count - action.payload };
+    case 'reset':
+      return initialState;
     default:
       return state;
   }
@@ -34,6 +43,7 @@ export const Counter = () => {
       <button onClick={() => dispatch({ type: 'decrement', payload: 10 })}>
         Decrement 10
       </button>
+      <button onClick={() => dispatch({ type: 'reset' })}>Reset</button>
     </>
   );
 };
