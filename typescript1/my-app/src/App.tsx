f1861514542c417f9b72d67ab6bda20fead8f704
Henry Ding <dinghenryr@gmail.com>
2023-06-24 04:44:18 +0000

    wrapping HTML elements, omit keyword.

--- typescript1/my-app/src/App.tsx
+++ typescript1/my-app/src/App.tsx
@@ -11,6 +11,7 @@ import { UserContextProvider } from './components/context/UserContext';
 import { List } from './components/generics/List';
 import { Greet } from './components/Greet';
 import { Heading } from './components/Heading';
+import { CustomButton } from './components/html/button';
 import { Input } from './components/Input';
 import { Oscar } from './components/Oscar';
 import { Person } from './components/Person';
@@ -100,6 +101,9 @@ function App() {
       />
       <RandomNumber value={10} isPositive />
       <Toast position="center" />
+      <CustomButton variant="primary" onClick={() => console.log('clicked')}>
+        Primary Button
+      </CustomButton>
     </div>
   );
 }
