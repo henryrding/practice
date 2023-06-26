c0d46ef287fdd378c8d644b29fe55bdc3ad1218a
Henry Ding <dinghenryr@gmail.com>
2023-06-26 07:20:08 +0000

    number, boolean, and type inference.

---
+++ typescript2/variable.ts
@@ -0,0 +1,13 @@
+const greetings = 'Hello World';
+
+console.log(greetings);
+
+// number, type inference
+const userId = 12345.67;
+userId.toFixed();
+
+// boolean
+const isLoggedIn = false;
+console.log(isLoggedIn);
+
+export {};
