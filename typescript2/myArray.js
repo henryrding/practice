1264f96fe83b33c659f599b32951a1d4d83d0719
Henry Ding <dinghenryr@gmail.com>
2023-06-27 06:00:13 +0000

    arrays.

---
+++ typescript2/myArray.js
@@ -0,0 +1,9 @@
+const superHeroes = [];
+// const heroPower: number[] = [];
+const heroPower = [];
+const allUsers = [];
+const MLModels = [[255, 255, 255], []];
+superHeroes.push('batman');
+heroPower.push(2);
+allUsers.push({ name: 'henry', isActive: true });
+MLModels.push([]);
---
+++ typescript2/myArray.ts
@@ -0,0 +1,17 @@
+const superHeroes: string[] = [];
+// const heroPower: number[] = [];
+const heroPower: Array<number> = [];
+
+type User = {
+  name: string;
+  isActive: boolean;
+};
+
+const allUsers: User[] = [];
+
+const MLModels: number[][] = [[255, 255, 255], []];
+
+superHeroes.push('batman');
+heroPower.push(2);
+allUsers.push({ name: 'henry', isActive: true });
+MLModels.push([]);
