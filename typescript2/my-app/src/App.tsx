c6acb22e30efb6aa865a5154bde8b2ed49141397
Henry Ding <dinghenryr@gmail.com>
2023-07-03 05:22:55 +0000

    classes in typescript.

--- typescript2/my-app/src/App.tsx
+++ typescript2/my-app/src/App.tsx
@@ -3,6 +3,17 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  class User {
+    email: string;
+    name: string;
+    city: string = '';
+    constructor(email: string, name: string) {
+      this.email = email;
+      this.name = name;
+    }
+  }
+
+  const henry = new User('henry@email.com', 'henry');
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +21,9 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <div>{henry.email}</div>
+        <div>{henry.name}</div>
+        <div>{(henry.city = 'riverside')}</div>
         <a
           className="App-link"
           href="https://reactjs.org"
