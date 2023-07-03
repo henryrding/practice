19e27478e848cc0fb0db8e0976b96b15e459461e
Henry Ding <dinghenryr@gmail.com>
2023-07-03 05:28:54 +0000

    private and public.

--- typescript2/my-app/src/App.tsx
+++ typescript2/my-app/src/App.tsx
@@ -2,18 +2,29 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// function App() {
+//   class User {
+//     public email: string;
+//     name: string;
+//     private city: string = 'Riveride';
+//     constructor(email: string, name: string) {
+//       this.email = email;
+//       this.name = name;
+//     }
+//   }
+
 function App() {
   class User {
-    email: string;
-    name: string;
-    city: string = '';
-    constructor(email: string, name: string) {
-      this.email = email;
-      this.name = name;
-    }
+    private readonly city: string = 'Riveride';
+    constructor(
+      public email: string,
+      public name: string
+    ) // private userId: string
+    {}
   }
 
   const henry = new User('henry@email.com', 'henry');
+
   return (
     <div className="App">
       <header className="App-header">
@@ -23,7 +34,7 @@ function App() {
         </p>
         <div>{henry.email}</div>
         <div>{henry.name}</div>
-        <div>{(henry.city = 'riverside')}</div>
+        {/* <div>{(henry.city)}</div> */}
         <a
           className="App-link"
           href="https://reactjs.org"
