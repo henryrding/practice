8d45588bafaf204e7536c6782f6e2d5964a6c36e
Henry Ding <dinghenryr@gmail.com>
2023-07-03 05:35:25 +0000

    getters and setters, private functions.

--- typescript2/my-app/src/App.tsx
+++ typescript2/my-app/src/App.tsx
@@ -2,25 +2,43 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-// function App() {
-//   class User {
-//     public email: string;
-//     name: string;
-//     private city: string = 'Riveride';
-//     constructor(email: string, name: string) {
-//       this.email = email;
-//       this.name = name;
-//     }
-//   }
-
 function App() {
+  //   class User {
+  //     public email: string;
+  //     name: string;
+  //     private city: string = 'Riveride';
+  //     constructor(email: string, name: string) {
+  //       this.email = email;
+  //       this.name = name;
+  //     }
+  //   }
+
   class User {
+    private _courseCount = 1;
+
     private readonly city: string = 'Riveride';
     constructor(
       public email: string,
-      public name: string
-    ) // private userId: string
-    {}
+      public name: string // private userId: string
+    ) {}
+    private deleteToken() {
+      console.log('token deleted');
+    }
+
+    get getAppleEmail(): string {
+      return `apple${this.email}`;
+    }
+
+    get courseCount(): number {
+      return this._courseCount;
+    }
+
+    set courseCount(courseNum) {
+      if (courseNum <= 1) {
+        throw new Error('Course count should be more than 1');
+      }
+      this._courseCount = courseNum;
+    }
   }
 
   const henry = new User('henry@email.com', 'henry');
