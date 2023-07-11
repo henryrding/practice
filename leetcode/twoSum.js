942fc11b0cc5615fa72c5e742de6430f20140e59
Henry Ding <dinghenryr@gmail.com>
2023-07-11 07:22:59 +0000

    added jest testing for twoSum and fixed order of indices of return array.

--- leetcode/twoSum.js
+++ leetcode/twoSum.js
@@ -6,25 +6,28 @@ export const twoSum = function (nums, target) {
   //         }
   //     }
   // }
+
   // for (let i = 0; i < nums.length - 1; i++) {
   //     const missing = target - nums[i]
   //     if (nums.indexOf(missing, i+1) !== -1) {
   //         return [i, nums.indexOf(missing, i+1)]
   //     }
   // }
-  // let map = new Map();
+
+  // const map = new Map();
   // for (let i = 0; i < nums.length; i++) {
-  //     let missing = target - nums[i];
+  //     const missing = target - nums[i];
   //     if (map.has(missing)) {
-  //         return [i, map.get(missing)]
+  //         return [map.get(missing), i]
   //     }
   //     map.set(nums[i], i);
   // }
+
   const map = {};
   for (let i = 0; i < nums.length; i++) {
     const missing = target - nums[i];
     if (missing in map) {
-      return [i, map[missing]];
+      return [map[missing], i];
     }
     map[nums[i]] = i;
   }
