fcf70177e79ee55103ab8d129436358db533386c
Henry Ding <dinghenryr@gmail.com>
2023-08-11 06:38:18 +0000

    completed gcdOfStrings and tests.

---
+++ leetcode/gcdOfStrings.js
@@ -0,0 +1,7 @@
+export const gcdOfStrings = function (str1, str2) {
+  if (str1 + str2 !== str2 + str1) return '';
+  const gcd = (a, b) => {
+    return b === 0 ? a : gcd(b, a % b);
+  };
+  return str1.substring(0, gcd(str1.length, str2.length));
+};
