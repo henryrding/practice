d2ead8eee69c41ed045a27a069fc5f3e575b4630
Henry Ding <dinghenryr@gmail.com>
2023-08-12 06:34:56 +0000

    completed increasingTripelet and tests.

---
+++ leetcode/increasingTriplet.js
@@ -0,0 +1,14 @@
+export const increasingTriplet = function (nums) {
+  let first = Number.MAX_SAFE_INTEGER;
+  let second = Number.MAX_SAFE_INTEGER;
+  for (const n of nums) {
+    if (n <= first) {
+      first = n;
+    } else if (n <= second) {
+      second = n;
+    } else {
+      return true;
+    }
+  }
+  return false;
+};
