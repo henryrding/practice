36b33a5a80cbcbee112ada2bd6eb0a61783d1996
Henry Ding <dinghenryr@gmail.com>
2023-08-14 04:57:18 +0000

    completed largestAltitude and tests.

---
+++ leetcode/largestAltitude.js
@@ -0,0 +1,11 @@
+export const largestAltitude = function (gain) {
+  let max = 0;
+  let current = 0;
+  for (let i = 0; i < gain.length; i++) {
+    current += gain[i];
+    if (current > max) {
+      max = current;
+    }
+  }
+  return max;
+};
