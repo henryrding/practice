de619d48e310c42a4e42b48c998036c0b1c96271
Henry Ding <dinghenryr@gmail.com>
2023-08-26 07:03:02 +0000

    completed mui box exercise.

--- mui/react-mui-app/src/components/MuiLayout.tsx
+++ mui/react-mui-app/src/components/MuiLayout.tsx
@@ -2,19 +2,29 @@ import { Box } from '@mui/material';
 
 export const MuiLayout = () => {
   return (
-    <Box
-      // component="span"
-      sx={{
-        backgroundColor: 'primary.main',
-        color: 'white',
-        height: '100px',
-        width: '100px',
-        padding: '16x',
-        '&:hover': {
-          backgroundColor: 'primary.light',
-        },
-      }}>
-      This is a box
-    </Box>
+    <>
+      <Box
+        // component="span"
+        sx={{
+          backgroundColor: 'primary.main',
+          color: 'white',
+          height: '100px',
+          width: '100px',
+          padding: '16x',
+          '&:hover': {
+            backgroundColor: 'primary.light',
+          },
+        }}>
+        This is a box
+      </Box>
+      <Box
+        display="flex"
+        height="100px"
+        width="100px"
+        bgcolor="success.light"
+        p={2}>
+        flex
+      </Box>
+    </>
   );
 };
