a06a942ca51f4259f86cbe8d805de0426a6d1b75
Henry Ding <dinghenryr@gmail.com>
2023-09-01 06:25:05 +0000

    updated productExceptSelf with other solutions.

--- leetcode/productExceptSelf.js
+++ leetcode/productExceptSelf.js
@@ -1,4 +1,26 @@
 export const productExceptSelf = function (nums) {
+  // const result = new Array(nums.length).fill(1);
+  // let fp = 1;
+  // let bp = 1;
+  // for (let i = 0, j = nums.length - 1; i < nums.length, j >= 0; i++, j--) {
+  //     result[i] *= fp;
+  //     result[j] *= bp;
+  //     fp *= nums[i];
+  //     bp *= nums[j];
+  // }
+  // return result;
+
+  // const result = new Array(nums.length).fill(1);
+  // let fp = 1;
+  // let bp = 1;
+  // for (let i = 0; i < nums.length; i++) {
+  //     result[i] *= fp;
+  //     result[nums.length - 1 - i] *= bp;
+  //     fp *= nums[i];
+  //     bp *= nums[nums.length - 1 - i];
+  // }
+  // return result;
+
   const answer = new Array(nums.length).fill(1);
   let leftProduct = 1;
   let rightProduct = 1;
